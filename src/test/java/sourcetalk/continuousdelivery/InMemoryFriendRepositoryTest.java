1cf559e5cab24d6c90cfd217441e3e3db6051f86
Waldemar Schneider <schneider@louis.info>
2013-10-02 14:52:29 +0200

    enhanced repository test

--- src/test/java/sourcetalk/continuousdelivery/InMemoryFriendRepositoryTest.java
+++ src/test/java/sourcetalk/continuousdelivery/InMemoryFriendRepositoryTest.java
@@ -10,6 +10,9 @@ public class InMemoryFriendRepositoryTest {
         InMemoryFriendRepository repository = new InMemoryFriendRepository();
         Friend friend = new Friend("Inspector", "Gadget");
         repository.addFriend(friend);
+        Friend friend1 = new Friend("Inspector", "Gadget");
+        repository.addFriend(friend1);
         assertTrue(repository.getFriends().contains(friend));
+        assertTrue(repository.getFriends().contains(friend1));
     }
 }
